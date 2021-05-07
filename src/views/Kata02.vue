2a3f3e1cfb8b733aa3b6c42ee5c2df7ccb0ea101
eMonk42 <lukas.dendtel@gmx.de>
2021-05-07 10:03:04 +0000

    kata2 barely functioonal in chrome at last

--- src/views/Kata02.vue
+++ src/views/Kata02.vue
@@ -1,10 +1,68 @@
 <template lang="">
-  <div>
+  <div class="kata2-wrapper">
     <h1>Kata2.vue here</h1>
+    <form @submit.prevent="submit">
+      <label for="input">Enter your query here!</label>
+      <input id="input" type="text" v-model="input" />
+      <button>Submit</button>
+    </form>
+    <div v-if="error != ''">{{ error }}</div>
+    <div v-if="isloading">Loading...</div>
+    <div v-if="!isloading">
+      <p>{{ wikiContent.extract }}</p>
+    </div>
     <router-link to="/">Home</router-link>
   </div>
 </template>
 <script>
-export default {};
+import wiki from "wikipedia";
+export default {
+  data() {
+    return {
+      input: "",
+      error: "",
+      isloading: false,
+      wikiContent: {}
+    };
+  },
+  methods: {
+    submit() {
+      console.log("button clicked");
+      this.getWiki();
+    },
+    async getWiki() {
+      this.error = "";
+      this.isloading = true;
+      try {
+        const page = await wiki.page(this.input);
+        console.log(page);
+        this.wikiContent = await page.summary();
+        console.log(this.wikiContent);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        this.isloading = false;
+      }
+    }
+  }
+};
 </script>
-<style lang=""></style>
+<style scoped lang="scss">
+.kata2-wrapper {
+  margin: 0 auto;
+  width: 50%;
+  form {
+    padding: 8rem 1rem;
+    display: flex;
+    flex-direction: column;
+    max-width: 15vw;
+    margin: 0 auto;
+    * {
+      padding: 1rem;
+    }
+    #input {
+      border: 1px solid magenta;
+    }
+  }
+}
+</style>
