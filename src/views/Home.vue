b6c790e32ee9dcc35a81f7844ac635cc17af91c9
eMonk42 <lukas.dendtel@gmx.de>
2021-05-07 08:56:03 +0000

    inittialization

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div> <h1>Hello I am the Home.vue file</h1> <Header /> </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
