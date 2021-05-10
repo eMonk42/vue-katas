6d79d33fc3b95bf05c53eeb2b8aab17e263ff15b
eMonk42 <lukas.dendtel@gmx.de>
2021-05-10 13:30:40 +0000

    date-kata basically functional

--- src/views/Kata05.vue
+++ src/views/Kata05.vue
@@ -1,10 +1,53 @@
 <template lang="">
-  <div>
+  <div class="wrapper">
     <h1>Kata5.vue here</h1>
+    <p>Did you ever wonder what time it might be?</p>
+    <div class="buttons">
+      <button @click="getTimeDe">Get german time</button>
+      <button @click="getTimeUs">Get US-time</button>
+    </div>
+    <p v-if="date != ''" id="output">{{ date }}</p>
     <router-link to="/">Home</router-link>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: ""
+    };
+  },
+  methods: {
+    getTimeDe() {
+      let currentDate = new Date();
+      this.date = currentDate
+        .toLocaleString("en-GB")
+        .replace("/", ".")
+        .replace("/", ".");
+    },
+    getTimeUs() {
+      let currentDate = new Date();
+      this.date = currentDate.toLocaleString("en-US"); //string + " in english";
+    }
+  }
+};
 </script>
-<style lang=""></style>
+<style scoped lang="scss">
+.wrapper {
+  max-width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .buttons {
+    button {
+      border: 1px solid magenta;
+      display: inline-block;
+      margin: 0 2rem;
+      padding: 1rem 1.5rem;
+    }
+    button:hover {
+      background-color: #333;
+    }
+  }
+}
+</style>
